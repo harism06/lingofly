a73ab0e4ae28401cb52fe8a4398d992d04a6147a
harism06 <hmalik6@cougarnet.uh.edu>
2025-09-06 12:03:19 -0500

    added back speaker button and pushed the instructions on diff page

---
+++ lingofly/Views/InstructionsPage.swift
@@ -0,0 +1,20 @@
+/*
+ Pilot (You): Los Alamitos Ground, Cessna N739KD, requesting taxi clearance to Runway 4R.
+ ATC Ground: Cessna N739KD, taxi to Runway 4R, via bravo, cross Runway 4L, hold short of Runway 4R.
+ Pilot (You): Taxi to Runway 4R via bravo, cross Runway 4L, hold short of Runway 4R, Cessna 9KD.
+ ATC Ground: Cessna 9KD, contact Tower 118.4.
+ Pilot (You): Over to Tower, Cessna 9KD.
+ Pilot (You): Los Alamitos Tower, Cessna N739KD holding short Runway 4R, ready for departure.
+ ATC Tower: Cessna 9KD, winds 224 at 3, Runway 4R cleared for takeoff. Fly runway heading after departure.
+ Pilot (You): Cleared for takeoff Runway 4R, fly runway heading, Cessna 9KD.
+ ATC Tower: Cessna 9KD, climb and maintain 2,500 feet.
+ Pilot (You): Climb and maintain 2,500, Cessna 9KD.
+ ATC Tower: Cessna 9KD, monitor Unicom 122.95. Bye bye.
+ Pilot (You): Wilco, switching to Unicom at 122.95, Cessna 9KD.
+ 
+ Plane Coordinates:
+ 1) x:0.15 y:0.775 deg: 11
+ 2) x:0.81 y:0.96 deg: 11
+ 3) x: 0.765 y: 0.84 deg: 11 -> 242
+ 4) x:0.43 y:0.1 deg: 242
+*/
